c5effd4b76db62431332a8e3b6034e7af22efb4c
aaplmath <aaplmath@users.noreply.github.com>
2016-01-25 16:06:28 -0500

    Added some much-needed comments and reorganized methods into a more logical order

--- Caffeinator/AppDelegate.swift
+++ Caffeinator/AppDelegate.swift
@@ -32,6 +32,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var sleepDisplay = true
     
+    // Add Notification Center observer to detect changes to the "display" preference, load the existing preference (or set one, true by default, if none exists), and set up the menu item
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         nc.addObserver(self, selector: "defaultsChanged", name: NSUserDefaultsDidChangeNotification, object: nil)
         if df.objectForKey("CaffeinateDisplay") != nil {
@@ -44,12 +45,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.menu = mainMenu
     }
     
+    // Terminate caffeinate upon application termination to prevent "zombie" processes
     func applicationWillTerminate(notification: NSNotification) {
         if let activeTask = task {
             activeTask.terminate()
         }
     }
     
+    // Responsible for managing the inactive/active state of the app. If there is an active "Caffeination," disable the appropriate menu items and set the icon green. Otherwise, enable all menu items and set the icon to the template.
     var active = false {
         didSet {
             startMenu.title = active ? "Stop Caffeinator" : "Start Caffeinator"
@@ -60,15 +63,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
-    
+    // Respond to a change to the CaffeinateDisplay default — this is used for both external updates (e.g., from Terminal using defaults write) and internal ones (see caffeinateDisplay())
     func defaultsChanged() {
         sleepDisplay = df.boolForKey("CaffeinateDisplay")
     }
     
+    // Respons to the Quit menu item
     @IBAction func quitClicked(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self)
     }
     
+    // Toggle the "display" option and update NSUserDefaults
     @IBAction func caffeinateDisplay(sender: NSMenuItem) {
         let val: Bool
         if sender.state == NSOnState {
@@ -82,6 +87,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         df.setBool(val, forKey: "CaffeinateDisplay")
     }
     
+    // Start Caffeinate with no args, or stop it if it is active
     @IBAction func startClicked(sender: NSMenuItem) {
         if sender.title == "Start Caffeinator" {
             generateCaffeine([], dev: false)
@@ -91,10 +97,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    // Responds to the "Run with args" item by opening the argument panel
     @IBAction func argumentClicked(sender: NSMenuItem) {
         argumentPanel.makeKeyAndOrderFront(nil)
     }
     
+    // Responds to the "Caffeinate process" item by prompting entry of a PID, which is passed alongside the corresponding "-w" argument to generateCaffeine()
     @IBAction func processClicked(sender: NSMenuItem) {
         if let res = inputDialog("Caffeinate a Process", title: "Select a Process", text: "Enter the PID of the process you would like to Caffeinate. This PID can be found in the Activity Monitor application:") {
             if let text = Int(res) {
@@ -105,6 +113,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    // Responds to the "Timed Caffeination" item by prompting time entry, then confirming valid input (generating errors as necessary) and passing the time (in seconds) to generateCaffeine() along with the corresponding "-t" argument
     @IBAction func timedClicked(sender: NSMenuItem) {
         let title = sender.title
         var time: Double? = nil
@@ -136,10 +145,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    // Responds to the "Help" item by opening the Help window
     @IBAction func helpPressed(sender: NSMenuItem) {
         helpHUD.makeKeyAndOrderFront(nil)
     }
     
+    // Generates an NSTask based on the arguments it is passed. If "dev" mode is not enabled (i.e., individual arguments have not been specified by the user), it will automatically add "-i" and, if the user has decided to Caffeinate their display, "-d"
     func generateCaffeine(var arguments: [String], dev: Bool) {
         if !dev {
             arguments.append("-i")
@@ -158,10 +169,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         active = true
     }
     
+    // Clean-up method that makes sure that the inactive state of the app is restored once caffeinate finishes running
     func taskDidTerminate(task: NSTask) {
         active = false
     }
     
+    // Remove Notification Center observer on deinit
     deinit {
         nc.removeObserver(self)
     }
@@ -177,32 +190,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var args: [String] = []
     var twArgs: [String: String] = [:]
     
-    @IBAction func confirmArguments(sender: NSButton) {
-        for (name, arg) in twArgs {
-            if let index = args.indexOf(name) {
-                args.insert(arg, atIndex: index + 1)
-            }
-        }
-        generateCaffeine(args, dev: true)
-        argumentPanel.close()
-    }
-    
-    @IBAction func cancelArguments(sender: NSButton) {
-        argumentPanel.close()
-    }
-    
-    @IBAction func addValue(sender: NSButton) {
-        let params = sender.tag == 0 ? ("-t", tLabel) : ("-w", wLabel)
-        if let value = showValueDialog(params.0) {
-            params.1.stringValue = value
-            twArgs[params.0] = value
-        }
-    }
-    
-    func showValueDialog(paramName: String) -> String? {
-        return inputDialog("Value Input", title: "Please Enter a Value", text: "Please enter the value for the \(paramName) parameter below:")
-    }
-    
+    // Responds to an argument being (un)checked by adding it to/removing it from the args array, and if it allows a manually-input value, enable/disable the corresponding input button
     @IBAction func argumentChecked(sender: NSButton) {
         let title = sender.title
         let state = sender.state == NSOnState
@@ -220,8 +208,39 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    // Shows the value input dialog and uses its return value for the corresponding argument, as determined by the sender's tag. These values are stored in "twArgs" so they can easily be added/removed until confirmArguments() is called
+    @IBAction func addValue(sender: NSButton) {
+        let params = sender.tag == 0 ? ("-t", tLabel) : ("-w", wLabel)
+        if let value = showValueDialog(params.0) {
+            params.1.stringValue = value
+            twArgs[params.0] = value
+        }
+    }
+    
+    // Displays a value input dialog for use in addValue(). Not to be confused with inputDialog()
+    func showValueDialog(paramName: String) -> String? {
+        return inputDialog("Value Input", title: "Please Enter a Value", text: "Please enter the value for the \(paramName) parameter below:")
+    }
+    
+    // Merge twArgs into the appropriate locations (directly after the corresponding argument) in args, then call generateCaffeine() in dev mode with args
+    @IBAction func confirmArguments(sender: NSButton) {
+        for (name, arg) in twArgs {
+            if let index = args.indexOf(name) {
+                args.insert(arg, atIndex: index + 1)
+            }
+        }
+        generateCaffeine(args, dev: true)
+        argumentPanel.close()
+    }
+    
+    // Close the argument panel
+    @IBAction func cancelArguments(sender: NSButton) {
+        argumentPanel.close()
+    }
+    
     // MARK: - Utility Alert Functions
     
+    // Show a two-button text input dialog to the user and returns the String result if the user presses OK. Not to be confused with showValueDialog()
     func inputDialog(windowTitle: String, title: String, text: String) -> String? {
         let alert = NSAlert()
         alert.window.title = windowTitle
@@ -239,6 +258,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return nil
     }
     
+    // Shows an error message with the specified text
     func errorMessage(title: String, text: String) {
         let alert = NSAlert()
         alert.window.title = "Error"
